73ea10ddbfe3a11c7aaffdc3d6d80ece747314ec
rodrigo-medrano <rodrigo.mespa.123@gmail.com>
2025-03-24 16:18:47 -0400

    views

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProductController;
+use App\Models\Category;
 use App\Models\Product;
 use Illuminate\Support\Facades\Route;
 
@@ -15,10 +16,12 @@ Route::get('/uno',function(){
 Route::get('/dos/{nombre?}',function($nombre='Invitado'){
     return 'Hola '.$nombre;
 });
-
+Route::get('/prueba',function(){
+    return view('prueba.index');
+});
 Route::prefix('admin')->group(function(){
     Route::get('/',function(){
-        return 'Ingresando al panel de administrador';
+        return 'Ingresando al panel de admin';
     });
     Route::get('uno',function(){
         return 'Opción 1';
@@ -47,6 +50,75 @@ Route::prefix('products')->group(function(){
         return 'Actualizar producto';
     });
     Route::delete('{id}',[ProductController::class,'destroy']);
+});
+
+Route::prefix('ejercicios')->group(function(){
+    Route::get('1',function(){
+        $productos=Product::all();
+        return $productos;
+    });
+    Route::get('2',function(){
+        $categoria=Category::find(2);
+        $productos=$categoria->products;
+        return $productos;
+    });
+    Route::get('3',function(){
+        $producto=Product::find(2);
+        $categoria=$producto->category;
+        return $categoria;
+    });
+    Route::get('4',function(){
+        $productos=Product::with('category')->get();
+        return $productos;
+    });
+    Route::get('5',function(){
+        $productos=Product::orderBy('price','desc')->get();
+        return $productos;
+    });
+    Route::get('6',function(){
+        $categorias=Category::withCount('products')->get();
+        return $categorias;
+    });
+    Route::get('7',function(){
+        $productos=Product::where('price','>',50)->get();
+        return $productos;
+    });
+    Route::get('8',function(){
+        $productos=Product::where('name','like','%a%')->get();
+        return $productos;
+    });
+    Route::get('9',function(){
+        $producto=Product::orderBy('price','desc')->first();
+        return $producto;
+    });
+    Route::get('10',function(){
+        $categorias=Category::orderBy('name','asc')->get();
+        return $categorias;
+    });
+    Route::get('11/{id?}',function($id=1){
+        $categoria=Category::find($id);
+        return $categoria;
+    });
+    Route::get('12',function($id=1){
+        $categorias=Category::where('name','like', 'e%')->get();
+        return $categorias;
+    });
+    Route::get('13/{id?}',function($id=1){
+        $categoria=Category::find($id);
+        $productos=$categoria->products;
+        return $productos;
+    });
+    Route::get('14',function(){
+        $categorias=Category::has('products')->get();
+        return $categorias;
+    });
+    Route::get('15',function(){
+        $categorias=Category::withCount('products')->orderBy('products_count','desc')->get();
+        return $categorias;
+    });
+    Route::get('/nombre/{id}/{nombre?}',function($id,$nombre='Invitado'){
+        return 'Hola '.$nombre.' tu id es '.$id;
+    });
 });
 
 
