f25cc9da5d2a2d7ef50c20d1a8d53fbd6df28b6b
rodrigo-medrano <rodrigo.mespa.123@gmail.com>
2025-05-02 17:08:22 -0400

    Authentication

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,17 +18,17 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         //User::factory(10)->create();
-        $this->call([
+        /*$this->call([
             CategorySeeder::class,
             ProductSeeder::class,
             //GenreSeeder::class,
             //MovieSeeder::class,
-        ]);
-
-
-        /*User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
         ]);*/
+
+
+        User::factory()->create([
+            'name' => 'Rodrigo Medrano',
+            'email' => 'rodrigo.mespa.123@gmail.com',
+        ]);
     }
 }
